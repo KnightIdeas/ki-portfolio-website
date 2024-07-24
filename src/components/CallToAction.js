07d5f7efa4b5d31cf9fa712c238c034d7ed557d7
Christopher Knight <110111479+KnightIdeas@users.noreply.github.com>
2024-07-24 11:36:37 +0100

    LinkButton and update to CallToAction
    I should start creating new branches for new components in the future and try to finish off at a set point.

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,19 +1,41 @@
 import React from "react";
+import LinkButton from "./LinkButton";
 import './CallToAction.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function CallToAction () {
     return (
+        <div className="footer-container">
         <section className="call-to-action">
             <h2>
-                Ready to Transform Your Business with AI?
+                Get in touch
             </h2>
-            <p> 
-                Unlock the full potential of your operations with custom AI solutions that drive growth and efficency.
-            </p>
             <button onClick={handleClick}>
                 Let's Collaborate
             </button>
+            <button>
+                Download CV
+            </button>
         </section>
+        <section className="footer-interior">
+            <div className="social-icons">
+                <LinkButton to='https://www.facebook.com/' external={true} className='social-icon'>
+                <FontAwesomeIcon icon={faFacebook} className="facebook-icon"/>
+                </LinkButton>
+                <LinkButton to='https://www.linkedin.com/in/' external={true} className='social-icon'>
+            <FontAwesomeIcon icon={faLinkedin} className="linkedin-icon"/>
+            </LinkButton>
+            <LinkButton to='https://www.github.com/' external={true} className='social-icon'>
+            <FontAwesomeIcon icon={faGithub} className="github-icon"/>
+            </LinkButton>
+            <LinkButton to='' external={true} className='social-icon'>
+            <FontAwesomeIcon icon={faEnvelope} className="envelope-icon"/>
+            </LinkButton>
+                </div> 
+        </section>
+        </div>
     );
 }
 
