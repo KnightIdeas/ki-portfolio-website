fae5344a3e99fdfb11b53b72c1047219a0b47bc0
Christopher Knight <110111479+KnightIdeas@users.noreply.github.com>
2024-10-15 14:41:57 +0100

    Header/ CV-download-added
    Add the CV a ref link to the header to allow for downloading of my CV
    Give the CV link the nav-link className to allow for css styling.
    Test the CV link for downloadability.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'; // Ensure this is linked properly
 import ModalContact from './ModalContact';
+import CurrentCV from '../assets/files/WebDevCV-ChristopherKnight-15-12-23.pdf';
 
 function Header() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -30,6 +31,7 @@ function Header() {
                 <Link to='/' className='nav-link'>Home</Link>
                 <Link to='/projects' className='nav-link'>Projects</Link>
                 <Link to='/' className='nav-link' onClick={handleContactClick}>Contact</Link>
+                <a href={CurrentCV} className='nav-link'>CV</a>
             </nav>
 
             {/* Mobile Navigation */}
@@ -38,6 +40,7 @@ function Header() {
                     <Link to='/' className='nav-link' onClick={() => setIsMobileMenuOpen(false)}>Home</Link>
                     <Link to='/projects' className='nav-link' onClick={() => setIsMobileMenuOpen(false)}>Projects</Link>
                     <Link to='/contact' className='nav-link' onClick={handleContactClick}>Contact</Link>
+                    <a href={CurrentCV} className='nav-link' onClick={() => setIsMobileMenuOpen(false)}>CV</a>
                 </nav>
             )}
             <ModalContact isOpen={isModalOpen} onClose={handleCloseModal} />
