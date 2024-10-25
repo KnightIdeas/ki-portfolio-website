1574d3834d3cc821f8c4758f31c3f63787d91695
Christopher Knight <110111479+KnightIdeas@users.noreply.github.com>
2024-10-25 14:34:20 +0100

    CV: Update CV for the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import ModalContact from './ModalContact';
-import CurrentCV from '../assets/files/WebDevCV-ChristopherKnight-15-12-23.pdf'; // CV file
+import CurrentCV from '../assets/files/Web Development CV 25-10-2024'; // CV file
 
 function Header() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
