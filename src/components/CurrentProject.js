8ff182d87e0ea5c4ef1e8258e5225edc60e7e9e6
Christopher Knight <chris@knightideas.co.uk>
2024-11-25 07:03:42 +0000

    CurrentProject: Add constructors and annote basic functions
    Add constructors for both MileStones and Images
    Outline the basic functions for both of these

---
+++ src/components/CurrentProject.js
@@ -0,0 +1,36 @@
+import { React, useState } from React;
+
+function CurrentProject () {
+    
+    // Constructors for updatable elements
+    class mileStone {
+        constructor(id, text, completed) {
+            this.id = id;
+            this.text = text;
+            this.completed = completed;
+        }
+    }
+
+    class image {
+        constructor(src, alt, arrayPosition) {
+            this.src = src;
+            this.alt = alt;
+            this.arrayPosition = arrayPosition;
+        }
+    }
+
+    // Arrays
+    const mileStones = [];
+    const gallery = [];
+
+    // Method for adding a milestone
+
+
+    // Add milestones
+
+
+    // Method for adding images
+
+    
+    // Add images
+}
