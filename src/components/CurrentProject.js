441ae2505832010531bcb3f448836788f951caff
Christopher Knight <110111479+KnightIdeas@users.noreply.github.com>
2024-12-04 10:38:51 +0000

    CurrentProjects: Add milestones
    Add Milestones to the project page.
    Add update to the project page.

--- src/components/CurrentProject.js
+++ src/components/CurrentProject.js
@@ -38,7 +38,24 @@ function CurrentProject () {
     const updates = [];
 
     // Add milestones
-    mileStones.push(new MileStone(1, 'Fully respond to brief', false, [1,11,2024]));
+    mileStones.push(new MileStone(0, 'Precode Planning', false, [15,12,2024]));
+    mileStones.push(new MileStone(1, 'Initial Base Code Setup', false, [31,12,2024]));
+    mileStones.push(new MileStone(2, 'Base Functionality Coding', false, [15,1,2025]));
+    mileStones.push(new MileStone(3, 'Initial Testing', false, [31,1,2025]));
+    mileStones.push(new MileStone(4, 'First Draft UI Design', false, [10,2,2025]));
+    mileStones.push(new MileStone(5, 'User Interface Development', false, [25,2,2025]));
+    mileStones.push(new MileStone(6, 'Feedback on First UI Draft', false, [28,2,2025]));
+    mileStones.push(new MileStone(7, 'Add Data and Asynchronous Functionality', false, [15,3,2025]));
+    mileStones.push(new MileStone(8, 'Second Round of Testing', false, [25,3,2025]));
+    mileStones.push(new MileStone(9, 'Second Draft UI Design', false, [31,3,2025]));
+    mileStones.push(new MileStone(10, 'Implement Advanced Features', false, [20,4,2025]));
+    mileStones.push(new MileStone(11, 'Feedback on Second UI Draft', false, [25,4,2025]));
+    mileStones.push(new MileStone(12, 'Third Round of Testing', false, [30,4,2025]));
+    mileStones.push(new MileStone(13, 'Final UI Design Adjustments', false, [10,5,2025]));
+    mileStones.push(new MileStone(14, 'Bug Fixing and Optimization', false, [25,5,2025]));
+    mileStones.push(new MileStone(15, 'Prepare for Launch', false, [31,5,2025]));
+    mileStones.push(new MileStone(16, 'Launch and Monitor', false, [15,6,2025]));
+    mileStones.push(new MileStone(17, 'Post-Launch Enhancements', false, [30,6,2025]));
 
     // format the date for the milestone
     mileStones.forEach((milestone) => {
@@ -49,8 +66,7 @@ function CurrentProject () {
     gallery.push(new Image(TempImage,'image coming soon', 'temp image title'));
 
     // Method for adding updates
-    updates.push(new Update('This is an update',[1,11,2024],'This is update text',gallery[0]))
-    updates.push(new Update('This is an update',[1,11,2024],'This is update text',gallery[0]))
+    updates.push(new Update('Milestones added',[3,12,2024],'Added the first batch of milestones to the page.',gallery[0]))
 
     // Format the date for the updates
     updates.forEach((update) => {
