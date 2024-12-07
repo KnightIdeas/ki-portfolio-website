7a5152966ce8167058ef1356a1f11092896919da
Christopher Knight <110111479+KnightIdeas@users.noreply.github.com>
2024-12-07 15:47:16 +0000

    MileStones: Create a collapse system for the milestones
    Create a collapse system for the milestones.
    Add the css for the collapse system.

--- src/components/CurrentProject.js
+++ src/components/CurrentProject.js
@@ -11,16 +11,23 @@ function CurrentProject () {
     return (
         <div className="page-container">
             <h1 className="page-title">
-            Current Project
+            IdClash
             </h1>
             <div className="subtitle-container">
             <h2 className="section-title">
-            Page Subtitle
+            Developing a Thriving Debate Community with an Engaging and Social App Experience.
             </h2>
             </div>
             <section className="overview-container">
-                <h3>Overview Title</h3>
+                <h3>Project Overview</h3>
                 <p>Overview Text</p>
+                <ul>
+                    <li><strong>Live Debate Rooms: </strong>Real-time voice and video channels for dynamic debates.</li>
+                    <li><strong>Community Voting System: </strong>Users can vote on debate topics and evaluate debaters based on clarity, persuasiveness, and respectfulness.</li>
+                    <li><strong>User Profiles & Reputation Scores: </strong>Personalized profiles that showcase achievements, badges, and reputation within the community.</li>
+                    <li><strong>Interactive Spectating: </strong>Features that allow spectators to engage by commenting, voting, and participating in post-debate discussions.</li>
+                    <li><strong>Leaderboard & Achievements: </strong>Gamification elements to motivate participation and recognize top debaters.</li>
+                </ul>
                 <img />
             </section>
             <MileStone />
